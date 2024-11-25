76805f4c2d2ed5f5de70dcc0ba91d79a2d01754b
Luke <me@gh.lukep.org>
2024-11-25 17:50:02 +0800

    chore: refactor basic file structure

--- main.go
+++ main.go
@@ -627,22 +627,3 @@ func leftPadTo32Bytes(i *big.Int) []byte {
 	copy(padded[32-len(bytes):], bytes)
 	return padded
 }
-
-func banner() string {
-	b := "\n"
-	b += fmt.Sprintf("%s%s                                     %s\n", ansiCodes["invertOn"], ansiCodes["bold"], ansiCodes["reset"])
-	b += fmt.Sprintf("%s%s     io.finnet Key Recovery Tool     %s\n", ansiCodes["invertOn"], ansiCodes["bold"], ansiCodes["reset"])
-	b += fmt.Sprintf("%s%s               v5.0.0                %s\n", ansiCodes["invertOn"], ansiCodes["bold"], ansiCodes["reset"])
-	b += fmt.Sprintf("%s%s                                     %s\n", ansiCodes["invertOn"], ansiCodes["bold"], ansiCodes["reset"])
-	b += "\n"
-	return b
-}
-
-func errorBox(err error) string {
-	b := "\n"
-	b += fmt.Sprintf("%s%s         %s\n", ansiCodes["darkRedBG"], ansiCodes["bold"], ansiCodes["reset"])
-	b += fmt.Sprintf("%s%s  Error  %s  %s.\n", ansiCodes["darkRedBG"], ansiCodes["bold"], ansiCodes["reset"], err)
-	b += fmt.Sprintf("%s%s         %s\n", ansiCodes["darkRedBG"], ansiCodes["bold"], ansiCodes["reset"])
-	b += "\n"
-	return b
-}
